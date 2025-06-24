0c3bf838a3d441b1b2b7010ec4a3ae8ae51a01b4
Chente000 <vicenteunivz@gmail.com>
2025-06-23 22:21:02 -0400

    Añadido script build.sh para automatizar el superusuario

--- build.sh
+++ build.sh
@@ -6,3 +6,14 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+# Crear superusuario si la variable CREATE_SUPERUSER está definida y es 'True'
+# Las variables de entorno DJANGO_SUPERUSER_USERNAME, EMAIL y PASSWORD se leen automáticamente.
+if [[ "$CREATE_SUPERUSER" = "True" ]]; then
+    echo "Creando superusuario..."
+    python manage.py createsuperuser --no-input
+    echo "Superusuario creado."
+fi
+
+# El "Start Command" en Render se encargará de ejecutar Gunicorn.
+# Este script solo se encarga de los pasos de build y setup.
